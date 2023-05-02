1c719c73403999c565503f6bd00ecf6cc799f8db
Will Denny <sparky.wgd@gmail.com>
2023-05-02 18:59:29 -0400

    fixed error

--- src/ElementarySchool.java
+++ src/ElementarySchool.java
@@ -5,5 +5,5 @@ public class ElementarySchool extends School
         System.out.println("This is McHarg elementary school " +
                 "serving grades 3 through 6");
     }
-    public void de
+
 }
